bb8ccd9c115b6bbf0cb0d7b3e01ef13dbe1b1c62
Karim Rahman Hartono <xsis.karimr@xl.co.id>
2021-09-01 23:02:50 +0700

    initial commit part 2

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # KebumianBot
 A custom bot to learn bot building in Discord.
+
+Prerequisites
+Node 16.0.0
+
+Modules
+dotenv => environment variables
+axios  => http promises
+
+How to Setup
+1. Create .env file with KebumianBot directory
+2. Copy env.sample contents to a new .env file
+3. Adjust the environment variables according to your machine
+
+How To Run
+Run in terminal: node index.js
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 require('dotenv').config(); //initialize dotenv
 const Discord = require('discord.js'); //import discord.js
 
-const client = new Discord.Client(); //create new client
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  };
+  if (msg.content === 'soki') {
+    msg.reply('adalah maskot dufan');
+  }
+  if (msg.content === 'rendy') {
+    msg.reply('adalah maskot kemprat');
+  }
 });
 
 //make sure this line is the last line
