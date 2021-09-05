f84aa4c5c685d436fd83385c142f048c7cb953a7
Karim Rahman Hartono <karim.rahman@bukalapak.com>
2021-09-05 15:43:15 +0700

    remove unnecessary wrapping

--- app/usecase.js
+++ app/usecase.js
@@ -7,42 +7,35 @@ const COMMAND_WITH_KEYWORD = ['play', 'p', 'search', 's'];
 const scopes = ['https://www.googleapis.com/auth/youtubepartner'];
 
 async function mapRequest(input, msgInstance) {
-    let request = manageCommands(input);
 
-    if (COMMAND_WITH_KEYWORD.includes(request[0]) == true && request[1].length == 0) {
-        return msgInstance.reply(`You don't specify the keyword, dumbass`);
-    }
-
-    if (request[0] == 'p' || request[0] == 'play') {
-        gauth.authenticate(scopes)
-            .then(client => ytservice.getTopVideo(client, request[1])
-                .then(videoUrl => {
-                    return msgInstance.reply(`Playing  ${videoUrl}`);
-                }))
-            .catch(console.error);
-    } else if (request[0] == 's' || request[0] == 'search') {
-        gauth.authenticate(scopes)
-            .then(client => ytservice.searchVideo(client, request[1])
-                .then(videoTitles => {
-                    return msgInstance.reply(`Search Results: \n${addNewLines(videoTitles)}`);
-                }))
-            .catch(console.error);
-    } else if (request[0] == 'stop') {
-        return msgInstance.reply(`Please stop the music!`);
-    } else {
-        return msgInstance.reply(`Your command is not registered`);
-    }
-
-}
-
-function manageCommands(input) {
     let deserialized = input.split(' ');
-
     let command = deserialized[0].substring(1);
     deserialized.shift();
     let keyword = combineKeywords(deserialized);
 
-    return [command, keyword];
+    if (COMMAND_WITH_KEYWORD.includes(command) == true && keyword.length == 0) {
+        return msgInstance.reply(`You don't specify the keyword, dumbass`);
+    }
+
+    if (command == 'p' || command == 'play') {
+        gauth.authenticate(scopes)
+            .then(client => ytservice.getTopVideo(client, keyword)
+                .then(videoUrl => {
+                    return msgInstance.reply(`Playing  ${videoUrl}`);
+                }))
+            .catch(console.error);
+    } else if (command == 's' || command == 'search') {
+        gauth.authenticate(scopes)
+            .then(client => ytservice.searchVideo(client, keyword)
+                .then(videoTitles => {
+                    return msgInstance.reply(`Search Results: \n${addNewLines(videoTitles)}`);
+                }))
+            .catch(console.error);
+    } else if (command == 'stop') {
+        return msgInstance.reply(`Please stop the music!`);
+    } else {
+        return msgInstance.reply(`Your command is not registered`);
+    }
 }
 
 function combineKeywords(wordArray) {
